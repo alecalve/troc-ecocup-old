4014fbaec6ec2f1621a3bad65afea518ecb8793e
alecalve <antoine@alc.io>
2013-05-27 18:20:13 +0200

    Plus besoin de javascript

--- view/head.php
+++ view/head.php
@@ -4,10 +4,11 @@
     <meta charset="utf-8">
     <title>Troc'Écocup</title>
     <link href="view/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="view/css/custom.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <div class="container">
         <div class="page-header">
-            <h1>Bienvenue sur Troc'Écocup <i>v2</i></h1>
+            <h1>Bienvenue sur <a href="index.php">Troc'Écocup <i>v2</i></a></h1>
             <p>Quoi de neuf <?php echo $_SESSION['user']; ?> ?</p>
         </div>
